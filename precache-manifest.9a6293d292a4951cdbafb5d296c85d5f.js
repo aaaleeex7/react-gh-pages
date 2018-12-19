c28a14d0383cd84d998678a0a62a5d41f084f775
Alexander Schübl <as333@hdm-stuttgart.de>
2018-12-19 17:42:12 +0100

    Updates

---
+++ precache-manifest.9a6293d292a4951cdbafb5d296c85d5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "2b37268e59e9cfcbe82d",
+    "url": "/react-gh-pages/static/js/main.2b37268e.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/react-gh-pages/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "2b37268e59e9cfcbe82d",
+    "url": "/react-gh-pages/static/css/main.7727c22e.chunk.css"
+  },
+  {
+    "revision": "3e1f23a66ca6271699be5cd62a40c0a2",
+    "url": "/react-gh-pages/index.html"
+  }
+];
